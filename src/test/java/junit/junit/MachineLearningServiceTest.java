8ca1510c188f381a7b42fb9bea826c859cb7c507
unknown <jmdccode@gmail.com>
2018-10-21 19:47:58 +0800

    asdf

--- src/test/java/junit/junit/MachineLearningServiceTest.java
+++ src/test/java/junit/junit/MachineLearningServiceTest.java
@@ -13,7 +13,7 @@ public class MachineLearningServiceTest {
 	@Test
 	public void test() {
 //		fail("Not yet implemented");
-		assertTrue(machineLearningService.isValid("false"));
+		assertTrue(machineLearningService.isValid("asdf"));
 	}
 
 }
